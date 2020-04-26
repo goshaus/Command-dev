4e204806ba29c86ca99d140717f9178538581596
yakubauskaitemarianna <mariannaraven@gmail.com>
2020-04-26 10:17:19 -0700

    Some improvements for converter

--- HtmlToPdf.Lib/Settings/HtmlToPdfSettings.cs
+++ HtmlToPdf.Lib/Settings/HtmlToPdfSettings.cs
@@ -2,7 +2,6 @@
 {
     public class HtmlToPdfSettings
     {
-        public string FileName { get; set; }
         public string Margin { get; set; }
         public PdfStandart Standart { get; set; }
         public PdfPageSize PageSize { get; set; }
