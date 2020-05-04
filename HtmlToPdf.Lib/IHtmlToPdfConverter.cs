73085401bf86c36e9d9aad3c7c1b42502389b788
yakubauskaitemarianna <mariannaraven@gmail.com>
2020-05-04 04:51:31 -0700

    Use autofac to resolve dependencies

---
+++ HtmlToPdf.Lib/IHtmlToPdfConverter.cs
@@ -0,0 +1,11 @@
+﻿using HtmlToPdf.Lib.Settings;
+using System.IO;
+
+namespace HtmlToPdf.Lib
+{
+    public interface IHtmlToPdfConverter
+    {
+        byte[] ConvertToPdfAsArray(HtmlToPdfSettings settings, string htmlString);
+        Stream ConvertToPdfAsStream(HtmlToPdfSettings settings, string htmlString);
+    }
+}
