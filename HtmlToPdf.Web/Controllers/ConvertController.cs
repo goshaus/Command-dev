8cae9d4fd27f1e5696c56d2707c1b37248ffb764
yakubauskaitemarianna <mariannaraven@gmail.com>
2020-05-05 05:37:20 -0700

    Let user to manage settings on main page

---
+++ HtmlToPdf.Web/Controllers/ConvertController.cs
@@ -0,0 +1,46 @@
+﻿using HtmlToPdf.Lib;
+using HtmlToPdf.Lib.Settings;
+using HtmlToPdf.Web.Infrastructure;
+using HtmlToPdf.Web.Models.Convert;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HtmlToPdf.Web.Controllers
+{
+    public class ConvertController : Controller
+    {
+        private readonly IHtmlToPdfConverter _converter;
+
+        public ConvertController(IHtmlToPdfConverter converter)
+        {
+            _converter = converter;
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(PopulateViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult Index(ConvertViewModel model)
+        {
+            return null;
+        }
+
+        private ConvertViewModel PopulateViewModel()
+        {
+            return new ConvertViewModel()
+            {
+                Settings = new SettingsViewModel()
+                {
+                    CompressionLevel = PdfCompressionLevel.Normal,
+                    PageOrientation = PdfPageOrientation.Portrait,
+                    PageSize = PdfPageSize.A4,
+                    Standart = PdfStandart.PdfA
+                }
+
+            };
+        }
+    }
+}
---
+++ HtmlToPdf.Web/Models/Convert/ConvertViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace HtmlToPdf.Web.Models.Convert
+{
+    public class ConvertViewModel
+    {
+        [Required, FileExtensions(Extensions = "html",
+             ErrorMessage = "Please specify .html file")]
+        public HttpPostedFileBase HtmlFile { get; set; }
+        public SettingsViewModel Settings { get; set; }
+    }
+}
